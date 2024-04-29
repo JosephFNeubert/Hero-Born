5f91b06124520f44043ff2465ac2acd019665d14
Joseph Neubert <joeyfneubert@gmail.com>
2024-04-29 08:14:14 -0700

    Created start screen w/ GUI

---
+++ Hero Born/Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameBehavior gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
+        gameManager.pause = true;
+        Time.timeScale = 0f;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+        gameManager.pause = false;
+        Time.timeScale = 1f;
+    }
+}
